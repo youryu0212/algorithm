3083cc70ea3b0e6117e8c6fbf0bb920f6f18e065
youryu0212 <youryu0212@naver.com>
2022-02-22 12:04:51 +0900

    solution : 숫자 카드 2 (이진탐색)

--- Binary_Search /javascript/10816.js
+++ Binary_Search /javascript/10816.js
@@ -1,39 +1,28 @@
-let leftIdx,rightIdx;
-const bisectLeft = (arr,num,minIdx,maxIdx) => {
-  if (minIdx > maxIdx) return -1;
-  const halfIdx = minIdx + Math.floor((maxIdx-minIdx)/2);
-  if (arr[halfIdx]>=num){
-    if (arr[halfIdx]===num) leftIdx = halfIdx;
-    bisectLeft(arr,num,minIdx,halfIdx-1);
-  }
-  else{
-    bisectLeft(arr,num,halfIdx+1,maxIdx);
-  }
-}
-const bisectRight = (arr,num,minIdx,maxIdx) => {
-  if (minIdx > maxIdx) return -1;
-  const halfIdx = minIdx + Math.floor((maxIdx-minIdx)/2);
-  if (arr[halfIdx]>num){
-    bisectRight(arr,num,minIdx,halfIdx-1);
-  }
-  else{
-    if (arr[halfIdx]===num) rightIdx = halfIdx;
-    bisectRight(arr,num,halfIdx+1,maxIdx);
-  }
-}
-
 const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-const n = Number(input[0]);
-const nums = input[1].split(' ').map(Number).sort((a,b)=>a-b);
-const arr = input[3].split(' ').map(Number)
+const card = input[1].split(' ').map(Number).sort((a,b)=>a-b);
+const arr = input[3].split(' ').map(Number);
+const bisectLeft = (left,right,target) => {
+  if (left >=right) return left;
+  const mid = left + Math.floor((right-left)/2);
+  if (card[mid] >= target) {
+    return bisectLeft(left,mid,target);
+  }
+  return bisectLeft(mid+1,right,target);
+  
+}
+const bisectRight = (left,right,target) => {
+  if (left >=right) return left-1;
+  const mid = left + Math.floor((right-left)/2);
+  if (card[mid] > target) {
+    return bisectRight(left,mid,target);
+  }
+  return bisectRight(mid+1,right,target);
+}
 
-const result = arr.reduce((acc,cur)=>{
-  leftIdx = -1 ,rightIdx = -1;
-  bisectLeft(nums,cur,0,n-1);
-  bisectRight(nums,cur,0,n-1);
-  if (leftIdx!==-1 && rightIdx!==-1) acc.push(rightIdx-leftIdx+1);
-  else acc.push(0);
+
+console.log(arr.reduce((acc,target)=>{
+  const left =  bisectLeft(0,card.length,target);
+  const right = bisectRight(0,card.length,target);
+  acc.push(right-left+1);
   return acc;
-},[]);
-
-console.log(result.join(" "));
+},[]).join(" "));
