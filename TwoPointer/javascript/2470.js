9042bfdddb8e203e1b98fa5ae4b4b4cc44c4bbf1
youryu0212 <youryu0212@naver.com>
2022-07-31 20:22:27 +0900

    solution: 2470 두 용액 (투포인터)

---
+++ TwoPointer/javascript/2470.js
@@ -0,0 +1,41 @@
+const [_, arr] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .split("\n")
+  .map((row) => row.split(" ").map(Number));
+
+const search = (arr) => {
+  const answer = [0, 0];
+  let [left, right] = [0, arr.length - 1];
+  let diff = Infinity;
+
+  arr.sort((a, b) => a - b);
+  while (left < right) {
+    const sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]].join(" ");
+    }
+    if (sum > 0) {
+      if (diff > Math.abs(sum)) {
+        diff = Math.abs(sum);
+        answer[0] = arr[left];
+        answer[1] = arr[right];
+      }
+      right -= 1;
+      continue;
+    }
+    if (sum < 0) {
+      if (diff > Math.abs(sum)) {
+        diff = Math.abs(sum);
+        answer[0] = arr[left];
+        answer[1] = arr[right];
+      }
+      left += 1;
+    }
+    continue;
+  }
+  return answer.join(" ");
+};
+
+const answer = search(arr);
+console.log(answer);
