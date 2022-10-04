6159564c5df682631e9410653dbd850a9afd21fd
hoi <youryu0212@naver.com>
2022-10-04 21:59:29 +0900

    solution: 1373 2진수 8진수 (문자열)

---
+++ String/javascript/1373.js
@@ -0,0 +1,19 @@
+const bin = [...require("fs").readFileSync("/dev/stdin").toString().trim()].map(
+  Number
+);
+const answer = [];
+let pow = 0;
+let num = 0;
+while (bin.length) {
+  num += 2 ** pow++ * bin.pop();
+  if (pow === 3) {
+    answer.push(num);
+    num = 0;
+    pow = 0;
+  }
+}
+
+if (num > 0) {
+  answer.push(num);
+}
+console.log(answer.reverse().join(""));
